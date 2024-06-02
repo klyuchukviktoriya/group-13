d2ccf5ed0888e32530d94a635b563048a06af089
Eva52975 <154532444+Eva52975@users.noreply.github.com>
2024-06-02 13:03:47 +0000

    Deploying to gh-pages from @ klyuchukviktoriya/group-13@b9ec3d95864d2dea53b78c19e2e6b29025a3f967 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,7 +1,12 @@
-import{a as i}from"./assets/vendor-bdb8a163.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const a=i.create({baseURL:"https://dummyjson.com"});async function l(){return await a.get("/products")}const u=o=>o.map(({id:r,title:c,price:n,description:e,tags:t,images:s})=>`<li class="productCard" data-id="${r}">
-    <img src="${s[0]}" alt="${t}" width="250">
-    <h2>${c}</h2>
+import{a as u}from"./assets/vendor-bdb8a163.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const d=u.create({baseURL:"https://dummyjson.com"});async function l(){return await d.get("/products")}async function i(r){return await d.get(`/products/${r}`)}const p=r=>r.map(({id:o,title:s,price:n,description:e,tags:t,images:c})=>`<li class="productCard" data-id="${o}">
+    <img src="${c[0]}" alt="${t}" width="250">
+    <h2>${s}</h2>
     <p>${n}</p>
     <p>${e}</p>
-  </li>`).join(""),d=document.getElementById("allProducts");l().then(({data:{products:o}})=>{console.log(o);const r=u(o);d.insertAdjacentHTML("beforeend",r)});
+  </li>`).join(""),m=({id:r,title:o,price:s,description:n,tags:e,images:t})=>`<div class="productCard" data-id="${r}">
+  <img src="${t[0]}" alt="${e}" width="250">
+  <h2>${o}</h2>
+  <p>${s}</p>
+  <p>${n}</p>
+</div>`,f=document.getElementById("allProducts"),a=document.getElementById("singleProductForm"),g=document.getElementById("singleProduct");l().then(({data:{products:r}})=>{console.log(r);const o=p(r);f.insertAdjacentHTML("beforeend",o)});a.addEventListener("submit",y);function y(r){r.preventDefault();const o=r.target.elements.id.value.trim();i(o).then(console.log),i(o).then(({data:s})=>{const n=m(s);g.innerHTML=n,a.reset()})}
 //# sourceMappingURL=commonHelpers.js.map
