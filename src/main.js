2745f39d0cb7d52ec71834b16f09f78e7db348ca
cel3ntato <adriano2024celentano@gmail.com>
2024-06-02 15:00:08 +0200

    search_by_id

--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
 // imports from backend
-import { getAllProducts } from "./requests/products";
-import { createProductsMarkup } from "./services/markupService"
+import { getAllProducts, getProductById } from './requests/products';
+import {
+  createProductsMarkup,
+  createProductsbyIdMarkup,
+} from './services/markupService';
 
 // Task 1
-const allProducts = document.getElementById("allProducts");
+const allProducts = document.getElementById('allProducts');
+// Task 2
+const form = document.getElementById('singleProductForm');
+const singleProduct = document.getElementById('singleProduct');
 
-getAllProducts()
-    .then(({ data: { products } }) => {
-        console.log(products);
-        const markupCard = createProductsMarkup(products);
-        allProducts.insertAdjacentHTML("beforeend", markupCard);
-    })
+getAllProducts().then(({ data: { products } }) => {
+  console.log(products);
+  const markupCard = createProductsMarkup(products);
+  allProducts.insertAdjacentHTML('beforeend', markupCard);
+});
 
-
-
+form.addEventListener('submit', searchById);
+function searchById(e) {
+  e.preventDefault();
+  const id = e.target.elements.id.value.trim();
+  getProductById(id).then(console.log);
+  getProductById(id).then(({ data }) => {
+    const markupCard = createProductsbyIdMarkup(data);
+    singleProduct.innerHTML = markupCard;
+    form.reset();
+  });
+}
